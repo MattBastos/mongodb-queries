ed903c25507902946163d97e81e99ee27a503363
Matthews Bastos <matthewsbasttos@gmail.com>
2023-02-07 18:42:14 -0300

    feat: desafio 10

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,1 +1,5 @@
-db.produtos.find({ valoresNutricionais: { $elemMatch: { tipo: "calorias", quantidade: { $lt: 500 } } } }, { _id: false, nome: true });
+db.produtos.find(
+  { valoresNutricionais: { $elemMatch: {
+    tipo: "calorias", quantidade: { $lt: 500 } } } },
+  { _id: false, nome: true },
+);
